2c24f38f64a3eb3f8b9cd167428f679b48db77c4
idk-codingal <mokshnagda2020@gmail.com>
2024-12-11 16:41:56 -0800

    first commit

---
+++ main2.py
@@ -0,0 +1,9 @@
+t1 = 98
+t2 = 94
+t3 = 41
+t4 = 400
+t5 = 190
+sum = t1+t2+t3+t4+t5
+print("The sum of all the trees is: ", sum)
+average = sum/5
+print("The average of all the trees is: ", average)
